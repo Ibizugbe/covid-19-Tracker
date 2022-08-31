0db28ae1dece55ebd95516d14628289fed64c461
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-08-31 14:51:22 +0100

    add nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,22 @@ function Nav() {
     <>
       <header>
         <nav>
-          <ul></ul>
+          <ul>
+            <li className="nav-link">
+              {
+                window.location.pathname === '/' 
+                ? (
+                  <span>Home</span>
+                ): (
+                  <button>Back</button>
+                )
+              }
+            </li>
+            <li className="nav-link">
+              <h1>COVID Statitics</h1>
+            </li>
+            <li className="nav-link"></li>
+          </ul>
         </nav>
       </header>
     </>
